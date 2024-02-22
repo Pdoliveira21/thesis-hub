3f9cced550b46a4cd25a1186050bdc9ff2e8c35b
Pdoliveira21 <up201905427@fe.up.pt>
2024-02-22 14:28:15 +0000

    overview graph construction to external function

--- test/d3-timeline/temporal-graph.js
+++ test/d3-timeline/temporal-graph.js
@@ -32,9 +32,67 @@ function drag(simulation) {
       .on("end", dragended);
 }
 
-// function overviewGraph() {
-  
-// }
+function overviewSVG(nodes, links, {
+  width = 800,
+  height = 800,
+  nodeSize = 12,
+  outerGroup = "national teams",
+  innerGroup = "clubs",
+  color = d3.scaleOrdinal(d3.schemeCategory10),
+}) {
+  const simulation = d3.forceSimulation()
+        .force("collide", d3.forceCollide(nodeSize + 2))
+        .force("link", d3.forceLink().id(d => d.id).strength(d => d.value * 0.01))
+        .force("x", d3.forceX().x(d => d.group === outerGroup ? d.fx : 0).strength(d => d.group === outerGroup ? 1.0 : 0.01))
+        .force("y", d3.forceY().y(d => d.group === outerGroup ? d.fy : 0).strength(d => d.group === outerGroup ? 1.0 : 0.01));
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+
+    let link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.8)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+        .attr("stroke-width", d => Math.min(d.value * 0.75, nodeSize * 2));
+
+    let node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+        .attr("r", nodeSize)
+        .attr("fill", d => color(d.group))
+        // .attr("opacity", d => d.clubs_count === 0 ? 0.3 : 1)
+        // .on("mouseenter", (event, d) => highlightNodeNeighbors(event, d, overwiewLinks, node, link, (d) => d.clubs_count === 0))
+        // .on("mouseleave", () => resetNetworkStyle(node, link, (d) => d.clubs_count === 0))
+        // .on("click", clicked);
+        // .call(drag(simulation))
+        .call(node => node.append("title").text(d => d.id));
+
+      node.filter(d => d.group === innerGroup)
+        .call(drag(simulation));
+    
+      simulation.nodes(nodes);
+      simulation.force("link").links(links);
+      simulation.on("tick", () => {
+        link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      });
+
+      return svg.node();
+}
 
 
 function TemporalGraph(data, graphContainer, {
@@ -74,58 +132,8 @@ function TemporalGraph(data, graphContainer, {
   });
 
 
-  const simulation = d3.forceSimulation()
-        .force("collide", d3.forceCollide(nodeSize + 2))
-        .force("link", d3.forceLink().id(d => d.id).strength(d => d.value * 0.01))
-        .force("x", d3.forceX().x(d => d.group === outerGroup ? d.fx : 0).strength(d => d.group === outerGroup ? 1.0 : 0.01))
-        .force("y", d3.forceY().y(d => d.group === outerGroup ? d.fy : 0).strength(d => d.group === outerGroup ? 1.0 : 0.01));
-
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-
-    let link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.8)
-      .selectAll("line")
-      .data(overviewLinks)
-      .join("line")
-        .attr("stroke-width", d => Math.min(d.value * 0.75, nodeSize * 2));
-
-    let node = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 0)
-      .selectAll("circle")
-      .data(overviewNodes)
-      .join("circle")
-        .attr("r", d => d.group === outerGroup ? nodeSize * 2 : nodeSize)
-        .attr("fill", d => color(d.group))
-        // .attr("opacity", d => d.clubs_count === 0 ? 0.3 : 1)
-        // .on("mouseenter", (event, d) => highlightNodeNeighbors(event, d, overwiewLinks, node, link, (d) => d.clubs_count === 0))
-        // .on("mouseleave", () => resetNetworkStyle(node, link, (d) => d.clubs_count === 0))
-        // .on("click", clicked);
-        // .call(drag(simulation))
-        .call(node => node.append("title").text(d => d.id));
-
-      node.filter(d => d.group === innerGroup)
-        .call(drag(simulation));
-    
-      simulation.nodes(overviewNodes);
-      simulation.force("link").links(overviewLinks);
-      simulation.on("tick", () => {
-        link
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-      });
-    
-      document.getElementById(graphContainer).append(svg.node());
+  const graph = overviewSVG(overviewNodes, overviewLinks, { width, height, nodeSize, outerGroup, innerGroup, color });
+  document.getElementById(graphContainer).append(graph);
 }
 
 // (TODO): separate responsabilities in functions and files if possible to simplify logic

eb8359b03ae51017b42be059f39417469568fca3
Pdoliveira21 <up201905427@fe.up.pt>
2024-03-13 12:09:05 +0000

    add delay on details graph enter to mascarade stroke node entering

--- prototype/src/graphs/DetailGraph.js
+++ prototype/src/graphs/DetailGraph.js
@@ -164,13 +164,13 @@ class DetailGraph extends Graph {
               if (d.group === self.innerGroup) {
                 g.select("image")
                   .attr("transform", "scale(0)")
-                  .transition().duration(self.animationDuration * 0.4).ease(self.animationEase)
+                  .transition().delay(100).duration(self.animationDuration * 0.4).ease(self.animationEase)
                   .attr("transform", "scale(1)");
               }
             } else {
               g.append("circle")
                 .attr("r", 0)
-                .transition().duration(self.animationDuration * 0.4).ease(self.animationEase)
+                .transition().delay(100).duration(self.animationDuration * 0.4).ease(self.animationEase)
                 .attr("r", radius)
                 .attr("fill", color);
               g.append("text")

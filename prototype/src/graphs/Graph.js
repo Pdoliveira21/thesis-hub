e2d476a0ac28e2e8cf19bad369d7c1083afa729a
Pdoliveira21 <up201905427@fe.up.pt>
2024-03-22 16:10:18 +0000

    fix firefox bug - remove raise and lower functions (not needed)

--- prototype/src/graphs/Graph.js
+++ prototype/src/graphs/Graph.js
@@ -89,11 +89,11 @@ export class Graph {
     // Highlight (change oppacity and display labels) of the node and its neighbors as well as corresponding links.
     let neighbors = new Set(link.data().filter(l => l.source === d || l.target === d).flatMap(l => [l.source, l.target]));
     node.filter(n => !neighbors.has(n)).call(g => {
-      g.lower().attr("opacity", this.nodeUnhighlightOpacity);
+      g.attr("opacity", this.nodeUnhighlightOpacity);
       g.select("text").attr("display", "none");
     });
     node.filter(n => neighbors.has(n)).call(g => {
-      g.raise().attr("opacity", this.nodeOpacity);
+      g.attr("opacity", this.nodeOpacity);
       g.select("text").attr("display", "block");
     });
 

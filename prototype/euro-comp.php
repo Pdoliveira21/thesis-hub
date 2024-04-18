8658844f58734b496d8025095666113fd91c4957
Pdoliveira21 <up201905427@fe.up.pt>
2024-04-18 11:41:09 +0100

    add header and footer in euro template

--- prototype/euro-comp.php
+++ prototype/euro-comp.php
@@ -4,13 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Campeonatos Europeus</title>
+  <link rel="stylesheet" href="./src/app.css" >
 </head>
 
 <body>
+  <?php 
+    $h1 = "Percurso de jogadores em seleções e equipas ao longo do tempo";
+    $description = "Esta nova visualização oferece uma perspectiva única sobre como os jogadores evoluem e contribuem para os campeonatos europeus do futebol.";
+    
+    include "./src/header/header.php"; 
+  ?>
+
   <?php
     $includePath = "./src/infovis";
 
-    $title = "Campeonatos da Europa";
     $endpoint = "https://www.zerozero.pt/api/v1/getGraphPlayersTeamsCompet/AppKey/tY9Qv2xP/competID/29/order/asc";
     $cacheFile = "./data/euro-comp.json";
     $dataPath = "data.seasons";
@@ -30,4 +37,6 @@
 
     include "./src/infovis/infographic.php"; 
   ?>
+
+  <?php include "./src/footer/footer.php"; ?>
 </body>
